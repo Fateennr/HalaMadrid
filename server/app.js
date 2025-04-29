76e8d1ccac7a98f1376d1329d6491919710456ec
NafisFuad106 <107258321+NafisFuadShahid@users.noreply.github.com>
2025-04-29 19:50:37 +0600

    feat: tried to fetch player stats from external api

--- server/app.js
+++ server/app.js
@@ -3,14 +3,17 @@ const express  = require("express");
 const cors     = require("cors");
 const newsR    = require("./routes/news");
 const matchR   = require("./routes/matches");
-const squadR   = require("./routes/squad");    
+const squadR   = require("./routes/squad"); 
+const playerR = require("./routes/players");   
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/news", newsR);
 app.use("/api/matches", matchR);
-app.use("/api/squad", squadR);    
+app.use("/api/squad", squadR);  
+app.use("/api/players", playerR);  
+
 
 
 
