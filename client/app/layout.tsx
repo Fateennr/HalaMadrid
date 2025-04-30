a43bffbabe926ec84e3766c044dd67a252763e4c
NafisFuad106 <107258321+NafisFuadShahid@users.noreply.github.com>
2025-05-01 04:31:15 +0600

    feat: redesign the fan-page for design consistency

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
     <html lang="en">
           <body>
             <Header />
+            <div className="h-14" />
               {children}
             <Footer />
           </body>
